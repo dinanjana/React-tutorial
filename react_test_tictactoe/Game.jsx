620d7520558ede726952ebef00cf46375355f991
dinanjana <dinanjanagunaratne@gmail.com>
2016-10-26 12:10:21 +0530

    modified:   react_test_tictactoe/Game.jsx

--- react_test_tictactoe/Game.jsx
+++ react_test_tictactoe/Game.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
+
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-    	{this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+    	{this.props.value}
       </button>
     );
   }
@@ -22,13 +17,63 @@ class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
-    };
+      isXnext:true,
+    }; 
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(!this.state.isXnext){
+      squares[i] = 'O';  
+    }else{
+      squares[i] = 'X';
+    }
+    
+    this.setState({
+                    squares: squares,
+                    isXnext:!this.state.isXnext
+                  });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}/>;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
+
   render() {
-    const status = 'Next player: X';
+    const status1 = 'Next player: X';
+    const status2 = 'Next player: O';
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if(winner=='X' || winner=='O'){
+      status='Winner is '.concat(winner);
+    }
+    else if(this.state.isXnext){
+      status=status1;
+    }
+    else{
+      status=status2;
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -53,11 +98,12 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
